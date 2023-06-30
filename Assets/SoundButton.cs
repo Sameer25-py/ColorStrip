4ceb19713b5793aa7d3612a699b2084e70a4e083
Muhammad Sameer <sameergb25@outlook.com>
2023-06-30 21:31:45 +0500

    implement sound toggle

---
+++ Assets/SoundButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class SoundButton : MonoBehaviour
+    {
+        public Sprite Mute, UnMute;
+
+        public AudioSource AudioSource;
+
+        private Button _button;
+
+        private void OnEnable()
+        {
+            _button              = GetComponent<Button>();
+            _button.image.sprite = AudioSource.mute ? Mute : UnMute;
+            _button.onClick.AddListener(OnButtonClicked);
+        }
+
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(OnButtonClicked);
+        }
+
+        private void OnButtonClicked()
+        {
+            AudioSource.mute     = !AudioSource.mute;
+            _button.image.sprite = AudioSource.mute ? Mute : UnMute;
+        }
+    }
+}
