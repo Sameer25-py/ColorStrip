1b32dd8431ac9e5bf3f4b2151bb0534f6ef16f75
Muhammad Sameer <sameergb25@outlook.com>
2023-06-30 21:34:51 +0500

    implement loader

---
+++ Assets/Loader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DefaultNamespace
+{
+    public class Loader : MonoBehaviour
+    {
+        private IEnumerator Start()
+        {
+            float   elapsedTime   = 0f;
+            Vector3 startRotation = Vector3.zero;
+            Vector3 endRotation   = new Vector3(0f, 0f, -360f);
+            while (elapsedTime < 1f)
+            {
+                elapsedTime += Time.deltaTime / 2f;
+                Vector3 lerpedRotation = Vector3.Lerp(startRotation, endRotation, elapsedTime);
+                transform.rotation = Quaternion.Euler(lerpedRotation);
+
+                yield return new WaitForEndOfFrame();
+            }
+
+            SceneManager.LoadScene("Gameplay");
+        }
+    }
+}
